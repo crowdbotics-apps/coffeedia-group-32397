360b002c1c9851bf1e6ac714714e1c8dd2b85f51
Crowdbotics <team@crowdbotics.com>
2021-12-15 04:59:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "coffeedia_group_32397",
+  url: "https://coffeedia_group_32397.botics.co",
+  api: "https://coffeedia_group_32397.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
